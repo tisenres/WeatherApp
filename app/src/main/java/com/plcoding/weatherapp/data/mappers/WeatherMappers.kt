ee818d73a0bfa903ba4c3cfa60753067e3790920
tisenres <tisenres@gmail.com>
2024-02-03 20:13:42 +0500

    Data and DI

--- app/src/main/java/com/plcoding/weatherapp/data/mappers/WeatherMappers.kt
+++ app/src/main/java/com/plcoding/weatherapp/data/mappers/WeatherMappers.kt
@@ -1,10 +1,17 @@
 package com.plcoding.weatherapp.data.mappers
 
 import com.plcoding.weatherapp.data.remote.WeatherDataDto
+import com.plcoding.weatherapp.data.remote.WeatherDto
 import com.plcoding.weatherapp.domain.weather.WeatherData
+import com.plcoding.weatherapp.domain.weather.WeatherInfo
 import com.plcoding.weatherapp.domain.weather.WeatherType
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+
+data class IndexedWeatherData(
+    val index: Int,
+    val data: WeatherData
+)
 
 fun WeatherDataDto.toWeatherDataMap(): Map<Int, List<WeatherData>> {
     return time.mapIndexed { index, time ->
@@ -13,7 +20,8 @@ fun WeatherDataDto.toWeatherDataMap(): Map<Int, List<WeatherData>> {
         val windSpeed = windSpeeds[index]
         val pressure = pressures[index]
         val humidity = humidities[index]
-        WeatherData(
+        
+        val weatherData = WeatherData(
             time = LocalDateTime.parse(time, DateTimeFormatter.ISO_DATE_TIME),
             temperatureCelsius = temperature,
             windSpeed = windSpeed,
@@ -21,7 +29,26 @@ fun WeatherDataDto.toWeatherDataMap(): Map<Int, List<WeatherData>> {
             humidity = humidity,
             weatherType = WeatherType.fromWMO(weatherCode)
         )
+        IndexedWeatherData(
+            index = index,
+            data = weatherData
+        )
     }.groupBy {
-        it.time.dayOfMonth
+        it.index / 24
+    }.mapValues {
+        it.value.map { data -> data.data }
     }
+}
+
+fun WeatherDto.toWeatherInfo(): WeatherInfo {
+    val weatherDataMap = weatherData.toWeatherDataMap()
+    val now = LocalDateTime.now()
+    
+    return WeatherInfo(
+        weatherDataPerDay = weatherDataMap,
+        currentWeatherData = weatherDataMap[0]?.find {
+            val hour = if (now.minute < 30) now.hour else now.hour + 1
+            hour == it.time.hour
+        }
+    )
 }
